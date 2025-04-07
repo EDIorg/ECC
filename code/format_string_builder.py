1a2e28c430025cb9aef03313e5702009eaf8a0ca
Colin Smith <colin.smith@wisc.edu>
2025-04-07 13:47:33 -0700

    Add scripts for date/time format list generation
    Add scripts for generating the recommended list of date/time
    formats and the regular expressions used for congruence checking.

--- code/format_string_builder.py
+++ code/format_string_builder.py
@@ -9,62 +9,71 @@ recommended set of date and time format strings.
 import csv
 from itertools import permutations
 
-def generate_date_components():
+
+def generate_date_formats() -> list:
+    """
+    :returns: A list of format strings for all permutations of date year,
+        month, and day components.
+    """
     components = ["YYYY", "MM", "DD"]
     separators = ["", "/", "-"]
     format_strings = []
 
-    # Generate permutations of the components
     for perm in permutations(components):
-        # Generate combinations with consistent separators
         for sep in separators:
             format_string = f"{perm[0]}{sep}{perm[1]}{sep}{perm[2]}"
             format_strings.append(format_string)
 
+    # Additional dates not composable in the above way
+    format_strings.extend([
+        "YYYYDDD",
+        "YYYY/DDD",
+        "YYYY-DDD",
+        "DDDYYYY",
+        "DDD/YYYY",
+        "DDD-YYYY",
+    ])
+
     return format_strings
 
 
-
-def generate_date_formats():
-    # Define basic date and time components
-    # date_components = [
-    #     "YYYY-MM-DD",
-    #     "YYYYMMDD",
-    #     "YYYY-DDD",
-    #     "YYYYDDD"
-    # ]
-    date_components = generate_date_components()
-    time_components = ["hh:mm:ss.sss", "hh:mm:ss", "hhmmss.sss", "hhmmss",
-                       "hh:mm", "hhmm", "hh", ""]
-    timezone_components = ["Z", "+hh:mm", "+hhmm", "+hh", "-hh:mm", "-hhmm",
-                           "-hh", ""]
-    separators = ["T", " "]
+def generate_datetime_formats():
+    date_formats = generate_date_formats()
+    time_formats = [
+        "hh",
+        "hhmm",
+        "hhmmss",
+        "hhmmss.sss",
+        "hh:mm",
+        "hh:mm:ss",
+        "hh:mm:ss.sss",
+    ]
+    timezone_formats = [
+        "Z",
+        "+hh:mm",
+        "+hhmm",
+        "+hh",
+        "-hh:mm",
+        "-hhmm",
+        "-hh"
+    ]
+    date_and_time_separators = ["T", " "]
 
     formats = []
 
     # Generate combinations of date and time components
-    for date_component in date_components:
-        for time_component in time_components:
+    for date_component in date_formats:
+        for time_component in time_formats:
             if time_component:
-                for separator in separators:
-                    for tz_component in timezone_components:
+                for separator in date_and_time_separators:
+                    for tz_component in timezone_formats:
                         formats.append(
                             f"{date_component}{separator}{time_component}{tz_component}")
             else:
                 formats.append(date_component)
 
-    # Add additional formats without separators
-    for date_component in date_components:
-        for time_component in time_components:
-            if time_component:
-                for tz_component in timezone_components:
-                    formats.append(
-                        f"{date_component}{time_component}{tz_component}")
-
     # Additional specific formats
-    formats.extend([
-        "YYYY-MM", "YYYYMM"
-    ])
+    formats.extend(["YYYY-MM", "YYYYMM", "MMYYYY", "MM-YYYY"])
 
     return formats
 
@@ -72,7 +81,6 @@ def generate_date_formats():
 def generate_example_date(format):
     example = "1976-09-23"
     example_time = "11:11:11.888"
-    example_day = "250"
     example_yyyymmdd = "19760923"
     example_yyyyd = "1976-250"
     example_yyyyd_dd = "1976250"
@@ -81,7 +89,6 @@ def generate_example_date(format):
         "YYYY": "1976",
         "MM": "09",
         "DD": "23",
-        "DDD": example_day,
         "hh": "11",
         "mm": "11",
         "ss": "11",
@@ -104,13 +111,13 @@ def generate_example_date(format):
 def write_to_csv(formats, file_path):
     with open(file_path, mode='w', newline='') as file:
         csv_writer = csv.writer(file)
-        for i, format in enumerate(formats):
+        for _, format in enumerate(formats):
             example_date = generate_example_date(format)
-            csv_writer.writerow([i + 1, format, example_date])
+            csv_writer.writerow([format, example_date])
 
 
 def main():
-    formats = generate_date_formats()
+    formats = generate_datetime_formats()
     csv_file_path = '/Users/csmith/Data/ecc/dateTimeFormatString_list.csv'
     write_to_csv(formats, csv_file_path)
     print(
---
+++ code/format_string_parser.py
@@ -0,0 +1,291 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+""":Mod: format_string_parser
+
+:Synopsis:
+    Reads a list of datetime format strings and generates regular expressions
+:Author:
+    Duane Costa
+
+:Created:
+    4/3/2018
+"""
+
+from docopt import docopt
+import logging
+from parsimonious.nodes import Node, NodeVisitor
+from parsimonious.grammar import Grammar
+import sys
+
+logging.basicConfig(format='%(asctime)s %(levelname)s (%(name)s): %(message)s',
+                    datefmt='%Y-%m-%d %H:%M:%S%z',
+                    level=logging.INFO)
+
+logger = logging.getLogger('format_string_parser')
+
+
+class FormatStringFormatter(NodeVisitor):
+    """Visitor that turns a parse tree into a regular expression"""
+
+    def visit_validDateTime(self, node, visited_children):
+        return '^' + ''.join(visited_children) + '$'
+
+    def visit_monthBasedDateTime(self, node, visited_children):
+        return ''.join(visited_children)
+
+    def visit_dayBasedDateTime(self, node, visited_children):
+        return ''.join(visited_children)
+
+    def visit_dayBasedDateReversedDateTime(self, node, visited_children):
+        return ''.join(visited_children)
+
+    def visit_yearDayMonthDateTime(self, node, visited_children):
+        return ''.join(visited_children)
+
+    def visit_monthYearDayDateTime(self, node, visited_children):
+        return ''.join(visited_children)
+
+    def visit_monthDayYearDateTime(self, node, visited_children):
+        return ''.join(visited_children)
+
+    def visit_dayMonthYearDateTime(self, node, visited_children):
+        return ''.join(visited_children)
+
+    def visit_dayYearMonthDateTime(self, node, visited_children):
+        return ''.join(visited_children)
+
+    def visit_monthBasedDate(self, node, visited_children):
+        return ''.join(visited_children)
+
+    def visit_monthBasedDate(self, node, visited_children):
+        return ''.join(visited_children)
+
+    def visit_dayBasedDate(self, node, visited_children):
+        return ''.join(visited_children)
+
+    def visit_dayBasedDateReversed(self, node, visited_children):
+        return ''.join(visited_children)
+
+    def visit_monthYearDay(self, node, visited_children):
+        return ''.join(visited_children)
+
+    def visit_dayYearMonth(self, node, visited_children):
+        return ''.join(visited_children)
+
+    def visit_monthDayYear(self, node, visited_children):
+        return ''.join(visited_children)
+
+    def visit_dayMonthYear(self, node, visited_children):
+        return ''.join(visited_children)
+
+    def visit_yearDayMonth(self, node, visited_children):
+        return ''.join(visited_children)
+
+    def visit_timeNoMinutesNoOffset(self, node, visited_children):
+        return ''.join(visited_children)
+
+    def visit_timeNoMinutesWithOffset(self, node, visited_children):
+        return ''.join(visited_children)
+
+    def visit_timeNoSecondsNoOffset(self, node, visited_children):
+        return ''.join(visited_children)
+
+    def visit_timeNoSecondsWithOffset(self, node, visited_children):
+        return ''.join(visited_children)
+
+    def visit_timeNoOffset(self, node, visited_children):
+        return ''.join(visited_children)
+
+    def visit_timeWithOffset(self, node, visited_children):
+        return ''.join(visited_children)
+
+    def visit_time(self, node, visited_children):
+        return ''.join(visited_children)
+
+    def visit_timeZ(self, node, visited_children):
+        return ''.join(visited_children)
+
+    def visit_year(self, node, visited_children):
+        return r'(\d\d\d\d)'
+
+    def visit_month(self, node, visited_children):
+        return r'(01|02|03|04|05|06|07|08|09|10|11|12)'
+
+    def visit_yearMonth(self, node, visited_children):
+        return ''.join(visited_children)
+
+    def visit_dayOfMonth(self, node, visited_children):
+        return r'(0[1-9]|[1-2]\d|30|31)'
+
+    def visit_dateDash(self, node, visited_children):
+        return node.text
+
+    def visit_timeColon(self, node, visited_children):
+        return node.text
+
+    def visit_T(self, node, visited_children):
+        return node.text
+
+    def visit_hours(self, node, visited_children):
+        return r'([0-1]\d|2[0-4])'
+
+    def visit_minutes(self, node, visited_children):
+        return r'([0-5]\d)'
+
+    def visit_wholeSeconds(self, node, visited_children):
+        return r'([0-5]\d)'
+
+    def visit_fractionalSeconds(self, node, visited_children):
+        return r'(\.\d\d\d)'
+
+    def visit_secondsWithFraction(self, node, visited_children):
+        return ''.join(visited_children)
+
+    def visit_secondsWithOutFraction(self, node, visited_children):
+        return ''.join(visited_children)
+
+    def visit_seconds(self, node, visited_children):
+        return ''.join(visited_children)
+
+    def visit_dayOfYear(self, node, visited_children):
+        return r'[0-3]\d\d'
+
+    def visit_offsetOperator(self, node, visited_children):
+        return '\\' + node.text
+
+    def visit_offset(self, node, visited_children):
+        return ''.join(visited_children)
+
+    def visit_offsetHours(self, node, visited_children):
+        return ''.join(visited_children)
+
+    def visit_offsetHoursMinutes(self, node, visited_children):
+        return ''.join(visited_children)
+
+    def visit_Z(self, node, visited_children):
+        return node.text
+
+
+def format_string_visitor(format_string):
+    grammar = Grammar(
+        """
+        validDateTime = monthBasedDateTime / dayBasedDateTime / 
+                        yearDayMonthDateTime /  monthYearDayDateTime / 
+                        dayYearMonthDateTime / 
+                        monthDayYearDateTime /
+                        dayMonthYearDateTime /
+                        dayBasedDateReversedDateTime /
+                        monthBasedDate \
+                        / dayBasedDate / yearMonth / year / timeZ
+        monthBasedDateTime = monthBasedDate T time Z
+        dayBasedDateTime = dayBasedDate T time Z
+        dayBasedDateReversedDateTime = dayBasedDateReversed T time Z
+        yearDayMonthDateTime = yearDayMonth T time Z
+        monthYearDayDateTime = monthYearDay T time Z
+        dayYearMonthDateTime = dayYearMonth T time Z
+        monthDayYearDateTime = monthDayYear T time Z
+        dayMonthYearDateTime = dayMonthYear T time Z
+        
+        monthBasedDate = year dateDash month dateDash dayOfMonth
+        dayBasedDate = year dateDash dayOfYear
+        dayBasedDateReversed = dayOfYear dateDash year
+        yearDayMonth = year dateDash dayOfMonth dateDash month
+        monthYearDay = month dateDash year dateDash dayOfMonth
+        dayYearMonth = dayOfMonth dateDash year dateDash month
+        monthDayYear = month dateDash dayOfMonth dateDash year
+        dayMonthYear = dayOfMonth dateDash month dateDash year
+        
+        timeNoMinutesNoOffset = hours
+        timeNoMinutesWithOffset = hours offset
+        timeNoSecondsNoOffset = hours timeColon minutes
+        timeNoSecondsWithOffset = hours timeColon minutes offset
+        timeNoOffset = hours timeColon minutes timeColon seconds
+        timeWithOffset = hours timeColon minutes timeColon seconds offset
+        time = timeWithOffset / timeNoOffset / timeNoSecondsWithOffset / \
+               timeNoSecondsNoOffset / timeNoMinutesWithOffset / \
+               timeNoMinutesNoOffset
+        timeZ = time Z
+        
+        year = "YYYY"
+        month = "MM"
+        yearMonth = year dateDash month
+        dayOfMonth = ~r"(?!DDD)DD"
+        dateDash = ~"[-/]?"
+        timeColon = ~":?"
+        T = ~r"[T\s]?"
+        hours = "hh"
+        minutes = "mm"
+        wholeSeconds = "ss"
+        fractionalSeconds = ".sss"
+        secondsWithFraction = wholeSeconds fractionalSeconds
+        secondsWithOutFraction = wholeSeconds
+        seconds = secondsWithFraction / secondsWithOutFraction
+        dayOfYear = "DDD"
+        offsetOperator = ~r"[+-]"
+        offset = offsetHoursMinutes / offsetHours
+        offsetHours = offsetOperator hours
+        offsetHoursMinutes = offsetOperator hours timeColon minutes
+        Z = ~"Z?"   
+        """)
+
+    tree = grammar.parse(format_string)
+    result = FormatStringFormatter().visit(tree)
+    return result
+
+
+def main():
+    """
+    Reads a list of datetime format strings and generates regular expressions
+    corresponding to each format string.
+
+    Usage:
+        format_string_parser.py [-i | --input <input>]
+                                [-o | --output <output>]
+        format_string_parser.py -h | --help
+
+    Options:
+        -i --input    Input file to read format strings from
+        -o --output   Output results to file;
+                      the filename should have a .csv extension
+        -h --help     This page
+
+    """
+    args = docopt(str(main.__doc__))
+    # input = args['<input>']
+    input = "/Users/csmith/Data/ecc/dateTimeFormatString_list.csv"
+    # output = args['<output>']
+    output = "/Users/csmith/Data/ecc/dateTimeFormatString_regex.csv"
+
+    if input is None:
+        fin = sys.stdin
+    else:
+        fin = open(input, 'rt')
+
+    if output is None:
+        fout = sys.stdout
+    else:
+        fout = open(output, 'wt')
+
+    format_strings = fin.readlines()
+
+    if (input):
+        fin.close()
+
+    for line in format_strings:
+        stripped_line = line.strip()
+        line_parts = stripped_line.split(',')
+        format_string = line_parts[0]
+        logger.info("Parsing %s" % format_string)
+        regex_result = format_string_visitor(format_string)
+        output_line = "%s,%s\n" % (stripped_line, regex_result)
+        fout.write(output_line)
+
+    if (output):
+        fout.close()
+
+    logger.info("Finished program")
+
+
+if __name__ == "__main__":
+    main()
